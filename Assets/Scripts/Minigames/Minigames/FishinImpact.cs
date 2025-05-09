ab9a9ce66badaab8120164340e97406f2689aee3
Ewin Mathew <79726981+senorpapistas@users.noreply.github.com>
2025-05-09 02:31:31 -0700

    genshin wish simulator v2

---
+++ Assets/Scripts/Minigames/Minigames/FishinImpact.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishenImpact : MonoBehaviour
+{
+    public GameObject pullAnimation;
+    private Animator fishPull;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishPull = pullAnimation.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SinglePull()
+    {
+        if (Random.Range(1, 100) <= 75)
+        {
+            fishPull.SetTrigger("win");
+            gameObject.SetActive(false);
+        } 
+        else
+        {
+            fishPull.SetTrigger("lose");
+            gameObject.SetActive(false);
+        }
+    }
+}
