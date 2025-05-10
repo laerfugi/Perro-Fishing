19b42ac6107a0f564ba5f5939493170e354b96a4
Ewin Mathew <79726981+senorpapistas@users.noreply.github.com>
2025-05-09 20:32:34 -0700

    sussy new minigame 😈

---
+++ Assets/FNAF.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FNAF : MonoBehaviour
+{
+    Animator jumpscareAnimator;
+    bool started = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpscareAnimator = GetComponent<Animator>();
+        Minigame.Instance.Lose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!started)
+        {
+            if (Minigame.Instance.minigameState == MinigameState.Play)
+            {
+                jumpscareAnimator.SetTrigger("start");
+                started = true;
+            }
+        }
+        
+    }
+}
