cc8af01642af736cb8317393691486b801395701
Ewin Mathew <79726981+senorpapistas@users.noreply.github.com>
2025-05-12 15:29:22 -0700

    settings menu functionality

--- Assets/Scripts/UI/MainMenu/MM_UI.cs
+++ Assets/Scripts/UI/MainMenu/MM_UI.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MM_UI : MonoBehaviour
 {
     public string mainScene;
+    [SerializeField]
+    private GameObject mainScreen, settingsScreen, sfxSlider, musicSlider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sfxSlider.GetComponent<Slider>().value = AudioManager.Instance.soundVolume;
+        musicSlider.GetComponent<Slider>().value = AudioManager.Instance.musicVolume;
     }
 
     // Update is called once per frame
@@ -27,5 +30,29 @@ public class MM_UI : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void setSFX()
+    {
+        AudioManager.Instance.soundVolume = sfxSlider.GetComponent<Slider>().value;
+        AudioManager.Instance.SetSoundVolume(AudioManager.Instance.soundVolume);
+    }
+
+    public void setMusic()
+    {
+        AudioManager.Instance.musicVolume = musicSlider.GetComponent<Slider>().value;
+        AudioManager.Instance.SetMusicVolume(AudioManager.Instance.musicVolume);
+    }
+
+    public void openSettings()
+    {
+        settingsScreen.SetActive(true);
+        mainScreen.SetActive(false);
+    }
+
+    public void closeSettings()
+    {
+        mainScreen.SetActive(true);
+        settingsScreen.SetActive(false);
     }
 }
