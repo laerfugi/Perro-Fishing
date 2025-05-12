c1a58e38577e7f7314dcbc59d10d441a3d6f70a4
Ewin Mathew <79726981+senorpapistas@users.noreply.github.com>
2025-05-12 13:26:42 -0700

    sprites now shuffle in main menu

--- Assets/Scripts/UI/MainMenu/MMplayerAnim.cs
+++ Assets/Scripts/UI/MainMenu/MMplayerAnim.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MMplayerAnim : MonoBehaviour
 {
     GameObject player;
     Animator anim;
     Animator redAnim;
+    public Database db;
+    SpriteRenderer lgSprite, fishSprite;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         anim = player.GetComponentInChildren<Animator>();
         redAnim = GameObject.Find("Red Little Guy").GetComponentInChildren<Animator>();
+        lgSprite = GameObject.Find("Red Little Guy").GetComponentInChildren<SpriteRenderer>();
+        fishSprite = GameObject.Find("fuckassgoldfish").GetComponent<SpriteRenderer>();
         GameObject.Find("Green Little Guy").GetComponentInChildren<Animator>().SetBool("walk", true);
         GameObject.Find("Blue Little Guy").GetComponentInChildren<Animator>().SetBool("walk", true);
     }
@@ -42,5 +47,11 @@ public class MMplayerAnim : MonoBehaviour
     {
         anim.SetBool("walk", true);
         anim.SetBool("run", true);
+    }
+
+    public void setSprites()
+    {
+        lgSprite.sprite = db.littleGuyList[Random.Range(0, db.littleGuyList.Count)].icon;
+        fishSprite.sprite = db.fishList[Random.Range(0, db.fishList.Count)].icon;
     }
 }
