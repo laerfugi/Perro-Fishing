9f75c597b410b278d2858cc43a84a453f62c3ace
Ewin Mathew <79726981+senorpapistas@users.noreply.github.com>
2025-05-12 12:59:10 -0700

    added little guy animations to Main Menu

--- Assets/Scripts/UI/MainMenu/MMplayerAnim.cs
+++ Assets/Scripts/UI/MainMenu/MMplayerAnim.cs
@@ -6,11 +6,15 @@ public class MMplayerAnim : MonoBehaviour
 {
     GameObject player;
     Animator anim;
+    Animator redAnim;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         anim = player.GetComponentInChildren<Animator>();
+        redAnim = GameObject.Find("Red Little Guy").GetComponentInChildren<Animator>();
+        GameObject.Find("Green Little Guy").GetComponentInChildren<Animator>().SetBool("walk", true);
+        GameObject.Find("Blue Little Guy").GetComponentInChildren<Animator>().SetBool("walk", true);
     }
 
     public void setWalk()
@@ -18,10 +22,20 @@ public class MMplayerAnim : MonoBehaviour
         anim.SetBool("walk", true);
     }
 
+    public void setLittleGuyWalk()
+    {
+        redAnim.SetBool("walk", true);
+    }
+
     public void setIdle()
     {
         anim.SetBool("walk", false);
         anim.SetBool("run", false);
+    }
+
+    public void setLittleGuyIdle()
+    {
+        redAnim.SetBool("walk", false);
     }
 
     public void setRun()
